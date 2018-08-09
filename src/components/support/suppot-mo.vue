dc829d352782c72fea31f838fac13d3ec693af9a
Yukiookami <1073898183@qq.com>
2018-08-09 15:05:16 +0800

    获得支持完成

---
+++ src/components/support/suppot-mo.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <div v-for="(item,index) in mainTittleList" :key="index" class="FAQ-box">
+    <div class="tittle-box">
+      {{item.tittle}}
+    </div>
+    <div v-for="(secItem,secIndex) in answerTittleList[index]" :key="secIndex" class="answer-box">
+      <h3>{{answerTittleList[index][secIndex].tittle}}</h3>
+      <ul>
+        <li v-for="(answerItem,answerIndex) in answerList[index]" :key="answerIndex"
+            v-if="answerItem.show == 1">
+          {{answerItem.msg}}
+        </li>
+        <li v-for="(answerItem,answerIndex) in answerList" :key="answerIndex"
+            v-if="answerItem.show == 2">
+          {{answerItem.msg}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['answerTittleList', 'mainTittleList', 'answerList']
+}
+</script>
+
+<style>
+.FAQ-box {
+  margin-top: 3rem;
+  padding-bottom: 1rem;
+  text-align: left;
+  border-bottom: dashed rgba(0, 0, 0, .2) .5rem;
+}
+
+.tittle-box {
+  padding: .5rem 1rem .5rem 3rem;
+  border: 1px solid #353535;
+  border-radius: 20px;
+  font-size: 14px;
+}
+
+.answer-box h3 {
+  margin: 1.5rem 0 .5rem 0;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.answer-box ul {
+  padding: 0 0 0 1.3rem;
+}
+
+.answer-box li {
+  line-height: 3rem;
+}
+
+.answer-box,
+.tittle-box {
+  margin: 0 auto;
+  padding-left: 3rem;
+  width: 40vw;
+}
+
+@media (max-width: 979px) {
+  .answer-box,
+  .tittle-box {
+    width: 60vw;
+  }
+}
+
+@media (max-width: 666px) {
+  .answer-box,
+  .tittle-box {
+    width: 80vw;
+  }
+}
+</style>
